a9def593764cf3deb3979fb8478f315ebe6793ea
fantasyshrimp <fantasy.shrimp@gmail.com>
2023-07-03 17:17:09 +0900

    .

--- java-springboot/app/src/main/resources/static/index.js
+++ java-springboot/app/src/main/resources/static/index.js
@@ -13,3 +13,18 @@ modal.onclick = () => {
 modalContent.onclick = (event) => {
   event.stopPropagation();
 }
+
+
+const header = document.querySelector('header')
+const headerHolder = document.getElementById('header-holder');
+
+headerHolder.onclick = (event) => {
+  
+  if (headerHolder.checked) {
+    header.style.position = 'sticky'
+    console.log("고정시도!")
+  } else {
+    header.style.position = 'relative'
+    console.log("해제!")
+  }
+}
