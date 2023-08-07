11b7aa1416dcb771f6f1d8ac1b2cbe4ec266b352
fantasyshrimp <fantasy.shrimp@gmail.com>
2023-08-07 18:39:26 +0900

    fix: href jsp, setting jsp path

--- java-springboot/app/src/main/java/study/app/controller/JSPController.java
+++ java-springboot/app/src/main/java/study/app/controller/JSPController.java
@@ -2,14 +2,17 @@ package study.app.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 @RestController
 public class JSPController {
 
   @GetMapping("/testjsp")
-  public Object testjsp() {
+  public ModelAndView testjsp() {
     System.out.println("receive testjsp con.");
-
-    return "test";
+    
+    ModelAndView model = new ModelAndView("test");
+    
+    return model;
   }
 }
---
+++ python.py
@@ -0,0 +1,7 @@
+list = [1, 2, 3]
+
+for i in range(0, len(list)):
+    print(list[i])
+
+for thing in list:
+    print(thing)
